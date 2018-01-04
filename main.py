2069dd80cc374f6c0f74f2bf08473445a5681c31
Luca Bortolotti <optiluca@gmail.com>
2018-01-04 21:50:36 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import scipy.io as sio
+from binance.client import Client
+
+api_things = sio.loadmat("api_things.mat")
+
+client = Client(api_things["api_key"][0]
+, api_things["api_secret"][0]
+)
+
+info = client.get_exchange_info()
