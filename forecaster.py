ef0c48761ead368b5c91ab4a7ba7dd62797e7816
Fifth <alexisgoodenough@gmail.com>
2018-01-13 00:39:25 +0100

    first forecasting

--- forecaster.py
+++ forecaster.py
@@ -1,11 +1,24 @@
+import numpy as np
+import time
+
+
 class Forecaster:
     def __init__(self, haven_coin):
         self.haven_coin = haven_coin
 
     def forecast(self, coin, timestamp):
-        # timestamp or vector of timestamps
+        # timestamp (in future: vector of timestamps)
+        now = int(time.time())
+        dt = int(timestamp) - now
+        current_price = coin.price(self.haven_coin)
+        last_prices = coin.price_history(self.haven_coin, '1m', 10)
+        dPrice = current_price - last_prices[1]
+        future_price = current_price + dPrice * dt / (10 * 60)
         pass
 
-    def validate(self, coin, timestamps):
+    def validate(self, coin):
         # call with past timestamps to verify algorithm goodness
-        pass
+        # random sample timestamps
+        # do forecast
+        # check
+        return True
