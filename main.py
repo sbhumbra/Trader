a69b7b1fa3676e1f444962334d66c58ca3458719
Luca Bortolotti <optiluca@gmail.com>
2018-01-28 23:55:32 +0100

    Forecaster now returns.  Autotrader no longer runs in fake mode. Removed all API permissions.  Load API mat file.  TODO: Add concept of time.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import manager as M
-import keyboard
 import time
+
+import manager as M
 
 filename_coinstats = ''
 filename_portfolio = ''
 
 Autotrader = M.Manager(filename_coinstats=filename_coinstats, filename_portfolio=filename_portfolio)
+Autotrader.portfolio.deposit(70, 'NEO', 0.58941)
 BRun = True
 tAutotraderWait = 5  # minutes
 while BRun:
@@ -14,7 +15,7 @@ while BRun:
     for i in range(1, (tAutotraderWait * 60)):
         time.sleep(1)
         # listen for stop key
-        if keyboard.is_pressed('space'):
+        if False:  # keyboard.is_pressed('space'):
             print('buy lambo')
             BRun = False
             break
