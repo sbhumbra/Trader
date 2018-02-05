3514fab995ed67b6b4b3141c1546e295ee9501c2
Fifth <alexisgoodenough@gmail.com>
2018-02-05 23:25:46 +0100

    implemented calc return

--- portfolio.py
+++ portfolio.py
@@ -7,26 +7,43 @@ class Portfolio:
         self.current_liquid_funds = 0
         self.current_holdings = {}
         exchange.value_portfolio(self)
-        self.past_liquid_funds = self.current_liquid_funds
-        self.past_holdings = copy.deepcopy(self.current_holdings)
 
     def calculate_return(self, exchange):
-        # # Update portfolio value after trading
-        # exchange.value_portfolio(self)
-        #
-        # past_values = self.current_values
-        # past_portfolio_value = np.sum(past_values) + past_liquid_funds
-        #
-        # self.current_values = np.multiply(current_prices, num_coins_held)
-        # current_portfolio_value = np.sum(self.current_values) + self.total_liquid_funds
-        #
-        # relative_change_per_coin = 100 * np.divide(self.current_values - past_values, past_values)
-        # relative_change = 100 * (current_portfolio_value - past_portfolio_value) / past_portfolio_value
-        #
-        # print('Current portfolio value: ' + str(current_portfolio_value) + ' ; ' + str(relative_change) + ' %')
-        # print("Coins are " + str(self.list_of_coin_types))
-        # print("Current holding: " + str(self.current_values).strip('[]'))
-        # print("Previous holding: " + str(past_values).strip('[]'))
-        # print("Relative change: " + str(relative_change_per_coin).strip('[]'))
-        # print('')
+        # Update portfolio value after trading
+        # Get previous state
+        past_liquid_funds = self.current_liquid_funds
+        past_holdings = copy.deepcopy(self.current_holdings)
+
+        # Get previous total worth
+        past_values = np.asarray([past_holdings[coin_type]['val'] for coin_type in past_holdings
+                                  if not coin_type == exchange.haven_coin_type])
+        past_portfolio_value = np.sum(past_values) + past_liquid_funds
+
+        # Get new state
+        exchange.value_portfolio(self)
+
+        # Get current total worth
+        current_coins = [coin_type for coin_type in self.current_holdings
+                         if not coin_type == exchange.haven_coin_type]
+        current_values = np.asarray([self.current_holdings[coin_type]['val'] for coin_type in self.current_holdings
+                                     if not coin_type == exchange.haven_coin_type])
+        current_portfolio_value = np.sum(current_values) + self.current_liquid_funds
+
+        # Relative portfolio change
+        relative_change = 100 * (current_portfolio_value - past_portfolio_value) / past_portfolio_value
+
+        # Relative change per coin only makes sense for coins that we still hold
+        relative_change_per_coin = 100 * np.asarray([((self.current_holdings[coin_type]['val'] /
+                                                       past_holdings[coin_type]['val']) *
+                                                      (self.current_holdings[coin_type]['num'] /
+                                                       past_holdings[coin_type]['num']) - 1)
+                                                     if coin_type in past_holdings else np.nan
+                                                     for coin_type in self.current_holdings])
+        relative_change_per_coin = np.round(relative_change_per_coin*100)/100
+
+        print('Total change: ' + "{:.2f}".format(relative_change) + ' %')
+        print("Coins are " + str(current_coins))
+        print("Current holding: " + str(current_values).strip('[]'))
+        print("Relative change: " + str(relative_change_per_coin).strip('[]') + ' %')
+        print('')
         pass
